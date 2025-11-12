67bbac3385674e63229ec6aa2b79495edc40bb35
Alielghayesh16006572 <ghayeshps@gmail.com>
2025-11-12 15:23:04 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,23 @@
-#ifndef VISITOR_H
-#define VISITOR_H
-
+#ifndef VISITOR_H_INCLUDED
+#define VISITOR_H_INCLUDED
+#include <iostream>
 #include <string>
 using namespace std;
+class Visitor{
+private:
+    string visitorName;
+    int ticketsBought;
+public:
+    Visitor(string visitorName, int ticketsBought);
+    Vistior();
+    ~Visitor();
+    string setVisitorName(string visitorName);
+    string getVisitorName();
+    int setTicketsBought(int ticketsBought);
+    int getTicketsBought();
+    void displayInfo();
+};
 
-//define visitor class here
 
 
-
-
-
-
-
-#endif
+#endif // VISITOR_H_INCLUDED
