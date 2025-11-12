da9320c5c3b6408145fde9f80814763e6a9fa397
Alielghayesh16006572 <ghayeshps@gmail.com>
2025-11-12 15:21:56 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,26 @@
-#ifndef ENCLOSURE_H
-#define ENCLOSURE_H
-
+#ifndef ENCLOSURE_H_INCLUDED
+#define ENCLOSURE_H_INCLUDED
+#include <iostream>
+#include <string>
 #include "Animal.h"
+using namespace std;
+class Enclosure{
+private:
+    Animal* animals;
+    int capacity;
+    int currentCount;
+public:
+    Enclosure(int capacity,int currentCount);
+    Enclosure();
+    ~Enclosure();
+    int setCapacity(int capacity);
+    int getCapacity();
+    int setCurrentCount(int currentCount);
+    int getCurrentCount();
+    void addAnimal(Animal* a);
+    void displayAnimals();
 
-//define enclosure class here 
+};
 
 
-
-
-
-
-
-
-#endif
+#endif // ENCLOSURE_H_INCLUDED
